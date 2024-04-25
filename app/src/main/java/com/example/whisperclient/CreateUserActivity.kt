7ba611373ea2a1da266767c88133d8a5dad6a2d7
Le Thi Yen Nhi <nhile0084@gmail.com>
2024-04-25 14:32:39 +0900

    Create User done

--- app/src/main/java/com/example/whisperclient/CreateUserActivity.kt
+++ app/src/main/java/com/example/whisperclient/CreateUserActivity.kt
@@ -5,6 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.whisperclient.databinding.ActivityCreateUserBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class CreateUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateUserBinding
@@ -17,10 +27,77 @@ class CreateUserActivity : AppCompatActivity() {
 
         //Create User button を押す時
         binding.createButton.setOnClickListener {
+            if (binding.passwordEdit.text.toString() != binding.rePasswordEdit.text.toString()) {
+                // Hiển thị thông báo nếu mật khẩu không khớp
+                Toast.makeText(
+                    this@CreateUserActivity,
+                    "パスワードが一致しません",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                // HTTP接続用インスタンス生成
+                val client = OkHttpClient()
+                // JSON形式でパラメータを送るようデータ形式を設定
+                val mediaType: MediaType = "application/json; charset=utf-8".toMediaType()
+                // Bodyのデータ(APIに渡したいパラメータを設定)
+//            val requestBody = "{\"userId\":\"${binding.userIdEdit.text}\",\n\"userName\":\"${binding.userNameEdit.text}\",\n\"password\":\"${binding.passwordEdit.text}\"}"
+                val requestBody = """
+                {
+                    "userId" : "${binding.userIdEdit.text}",
+                    "userName" : "${binding.userNameEdit.text}",
+                    "password" : "${binding.passwordEdit.text}"
+                }
+            """.trimIndent()
+                println("Request Body: $requestBody")
+                // Requestを作成(先ほど設定したデータ形式とパラメータ情報をもとにリクエストデータを作成)
+                val request = Request.Builder().url("http://10.200.2.86/whisper/userAdd.php")
+                    .post(requestBody.toRequestBody(mediaType)).build()
+                println("Request: $request")
+                // リクエスト送信（非同期処理）
+                client.newCall(request).enqueue(object : Callback {
+                    // リクエストが失敗した場合の処理を実装
+                    override fun onFailure(call: Call, e: IOException) {
+                        // runOnUiThreadメソッドを使うことでUIを操作することができる。(postメソッドでも可)
+                        this@CreateUserActivity.runOnUiThread {
+//                        binding.tvJson.text = "リクエストが失敗しました: ${e.message}"
+                            println("Request failed: ${e.message}")
+                            Toast.makeText(
+                                this@CreateUserActivity,
+                                "\"Request failed: ${e.message}\"",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
 
-            //User作成できたら、、Timeline画面へ
-            val i = Intent(this, TimelineActivity::class.java)
-            startActivity(i)
+                    // リクエストが成功した場合の処理を実装
+                    override fun onResponse(call: Call, response: Response) {
+                        val responseBody = response.body?.string()
+                        val jsonResponse = JSONObject(responseBody)
+                        try {
+                            val result = jsonResponse.optString("result")
+                            if (result == "success") {
+                                val intent =
+                                    Intent(this@CreateUserActivity, TimelineActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                val errorMessage = jsonResponse.optString("errorMessage")
+                                throw IOException(errorMessage)
+                            }
+                        } catch (e: Exception) {
+                            runOnUiThread {
+                                val errorCode = jsonResponse.optString("errCode")
+                                val errorMessage = jsonResponse.optString("errMsg")
+//                            throw IOException("$errorCode: $errorMessage")
+                                Toast.makeText(
+                                    this@CreateUserActivity,
+                                    "$errorMessage",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                })
+            }
         }
 
         //Cancel button を押す時
