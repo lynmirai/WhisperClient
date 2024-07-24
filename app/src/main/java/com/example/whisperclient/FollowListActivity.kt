80450fd10872348da6dc91140a56ad8127159912
2220394 <130331053+2220394@users.noreply.github.com>
2024-07-24 10:04:30 +0900

    フォローリスト画面作成

--- app/src/main/java/com/example/whisperclient/FollowListActivity.kt
+++ app/src/main/java/com/example/whisperclient/FollowListActivity.kt
@@ -6,10 +6,25 @@ import android.os.Bundle
 import android.view.ContextMenu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.whisperclient.databinding.ActivityFollowListBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class FollowListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFollowListBinding
+    //recyclerViewの宣言
+    lateinit var srv : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_follow_list)
@@ -18,6 +33,128 @@ class FollowListActivity : AppCompatActivity() {
 
         //dang ky menu ngu canh
         registerForContextMenu(binding.imgMenu)
+
+        //recyclerViewをレイアウトと紐づける
+        srv = binding.followRecycle
+        srv.layoutManager = LinearLayoutManager(this)
+
+        //渡されてきたユーザIDとフォローステータスを受け取る
+        var userId = intent.getStringExtra("userId")
+        val followStatus = intent.getStringExtra("followStatus")
+        println("受け取ったゆーざID:$userId,フォロー状態:$followStatus")
+
+        //フォローならfollowListにテキストを変える
+        if(followStatus == "follow"){
+            binding.followListText.text = "followList"
+        }else if(followStatus == "follower"){
+            binding.followListText.text = "followerList"
+        }
+
+        // HTTP接続用インスタンス生成
+        val client = OkHttpClient()
+        // JSON形式でパラメータを送るようデータ形式を設定
+        val mediaType : MediaType = "application/json; charset=utf-8".toMediaType()
+        // Bodyのデータ(APIに渡したいパラメータを設定)
+        val requestBody = """
+                    {
+                        "userId":"$userId"
+                    }
+                    """.trimIndent()
+        println("Request Body: $requestBody")
+        // Requestを作成(先ほど設定したデータ形式とパラメータ情報をもとにリクエストデータを作成)
+        val request = Request.Builder().url("https://click.ecc.ac.jp/ecc/whisper24_d/followerInfo.php").post(requestBody.toRequestBody(mediaType)).build()
+        println("Request: $request")
+        //コールバック処理
+        client.newCall(request!!).enqueue(object  : Callback {
+            //リクエストが失敗したとき
+            override fun onFailure(call: Call, e: IOException) {
+                //エラーメッセージ表示
+                runOnUiThread {
+                    Toast.makeText(this@FollowListActivity,"リクエスト失敗:${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            //リクエストが成功したとき
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body?.string()
+                val jsonResponse = JSONObject(responseBody)
+                try{
+                    println("レスポンスを受信しました:$responseBody")
+                    val result = jsonResponse.optString("result")
+//                    if (result == "success") {
+                    if(followStatus == "follow"){
+                        //ユーザ情報を格納するリストを作成
+                        val users = mutableListOf<User>()
+                        //ユーザ情報をリストに格納する
+                        val userList = jsonResponse.optJSONArray("followList")
+                        if(userList != null){
+                            //ユーザ情報が存在する間ループ
+                            for(i in 0 until userList.length()){
+                                //
+                                val userJson = userList.getJSONObject(i)
+                                users.add(
+                                    User(
+                                        userName = userJson.optString("userName"),
+                                        follow = userJson.optInt("followcnt"),
+                                        follower = userJson.optInt("followercnt"),
+                                        userImg = R.drawable.ic_launcher_background,
+                                        userId = userJson.optString("userId")
+                                    )
+                                )
+                            }
+                        }
+                        //リサイクラービューにセットする
+                        runOnUiThread {
+                            val app = application as MyApplication
+                            srv.adapter = UserAdapter(app, users)
+                            srv.adapter?.notifyDataSetChanged() // これを追加してアダプタの変更を通知
+                        }
+                    }else{
+                        //ユーザ情報を格納するリストを作成
+                        val users = mutableListOf<User>()
+                        //ユーザ情報をリストに格納する
+                        val userList = jsonResponse.optJSONArray("followerList")
+                        if(userList != null){
+                            //ユーザ情報が存在する間ループ
+                            for(i in 0 until userList.length()){
+                                //
+                                val userJson = userList.getJSONObject(i)
+                                users.add(
+                                    User(
+                                        userName = userJson.optString("followerUser"),
+                                        follow = userJson.optInt("followcnt"),
+                                        follower = userJson.optInt("followercnt"),
+                                        userImg = R.drawable.ic_launcher_background,
+                                        userId = userJson.optString("userId")
+                                    )
+                                )
+                            }
+                        }
+                        //リサイクラービューにセットする
+                        runOnUiThread {
+                            val app = application as MyApplication
+                            srv.adapter = UserAdapter(app, users)
+                            srv.adapter?.notifyDataSetChanged() // これを追加してアダプタの変更を通知
+                        }
+                    }
+
+
+//                    } else {
+//                        //エラーメッセージ表示
+//                        val errorMessage = jsonResponse.optString("errorMessage")
+//                        throw IOException(errorMessage)
+//                    }
+                }catch (e:Exception){
+                    //JSONデータがエラーの場合
+                    val errorCode = jsonResponse.optString("errCode")
+                    val errorMessage = jsonResponse.optString("errMsg")
+                    runOnUiThread {
+                        Toast.makeText(this@FollowListActivity,"$errorMessage", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+
     }
 
     //
